ee4abbf5f9ca6e4ebad13422c6724d3488d1993e
t3j3ndrA <tejendradhanani@gmail.com>
2023-03-09 19:49:30 +0530

    get comanies for specified student api

--- backend/routes/company/company.route.js
+++ backend/routes/company/company.route.js
@@ -45,6 +45,30 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/of/:studentId", async (req, res) => {
+  const { studentId } = req.params;
+  const student = await Student.findOne({ _id: studentId });
+  if (!student) return res.json({ success: false, msg: "No student found" });
+
+  const companies = await Company.find({ forBatch: student.passingYear });
+  let resCompanies = [];
+  companies.forEach((c) => {
+    let roles = c.roles.map((role) => {
+      console.log("role... > ", { ...role });
+      if (role?.elligibles.includes(studentId)) {
+        return { ...role._doc, isElligible: true };
+      } else {
+        return { ...role._doc, isElligible: false };
+      }
+    });
+    if (roles) {
+      resCompanies.push({ ...c._doc, roles });
+    }
+  });
+
+  return res.json({ success: true, data: resCompanies });
+});
+
 // register new Company
 router.post("/new", async (req, res) => {
   const { name, website, email, forBatch, description, roles, address } =
