b01e0571e64074b3e944b9d6cc3d1233ef1bf43d
t3j3ndrA <tejendradhanani@gmail.com>
2023-03-26 10:45:33 +0530

    Login page clean up with toastify

--- student-front/src/components/Form/Login.jsx
+++ student-front/src/components/Form/Login.jsx
@@ -1,82 +1,131 @@
-import React from 'react'
-import loadImg from './1.jpeg'
-import { useState } from 'react'
+import React from "react";
+import loadImg from "./1.jpeg";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import { Homepage } from '../Homepage/Homepage';
-import { Register } from '../Register/Register';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { Homepage } from "../Homepage/Homepage";
+import { Register } from "../Register/Register";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
+import { BeatLoader } from "react-spinners";
 
 export const Login = () => {
-  const [Email, setEmail] = useState('')
-  const [Password, setPassword] = useState('')
-  const [AllEntry, setAllEntry] = useState({})
-  const navigate = useNavigate()
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+  const [AllEntry, setAllEntry] = useState({});
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
 
-  const addEntry = () =>{
-    const newEntry = {email: Email, password: Password}
-    setAllEntry({...AllEntry,newEntry})
-  }
+  const addEntry = () => {
+    const newEntry = { email: Email, password: Password };
+    setAllEntry({ ...AllEntry, newEntry });
+  };
 
-  let url = "http://127.0.0.1:5000/api"
-  const postEntry = async(e) =>{
+  const postEntry = async (e) => {
     e.preventDefault();
-    console.log("hello from method in login.jsx")
-    try{
+    setIsLoading(true);
+    try {
       const post = {
         email: Email,
-        password: Password
-      }
-      console.log(post)
+        password: Password,
+      };
+      console.log(post);
 
-      const res = await axios.post(`/api/auth/student/login`, post, {withCredentials : true})
-      console.log(res.data.success)
-      console.log(res.data)
-      if(res.data.success === true){
-        
-        console.log("response: " ,res.data.data)
+      const res = await axios.post(`/api/auth/student/login`, post, {
+        withCredentials: true,
+      });
+      if (res.data.success === true) {
         localStorage.setItem("stuId", res.data.data.studentId);
-        navigate('/Homepage') 
-        }else{
-          alert("Incorrect password or username")
-          navigate('/Login')
-        }
-          
-    }catch(e){
-      console.log("error: ", e)
+        navigate("/Homepage");
+      } else {
+        toast.error("Invalid credentials");
+        // alert("Incorrect password or username");
+        navigate("/Login");
+      }
+    } catch (e) {
+      console.log("error: ", e);
     }
-
-  }
+    setIsLoading(false);
+  };
 
   return (
-    <div className='grid grid-cols-1 sm:grid-cols-2 h-screen w-full'>
-      <div className='bg-gray-800 flex flex-col justify-center'>
-        <form className='max-w-[400px] w-full mx-auto bg-gray-900 p-8 px-8 rounded-lg' action={addEntry} method = "post">
-          <h2 className='text-4xl dark:text-white font-bold text-center'>
-            Login
-          </h2>
-          <div className='flex flex-col text-gray-400 py-2'>
-            <label htmlFor='email'>Email</label>
-            <input type="email" className='rounded-lg mt-2 pt-2 bg-gray-700 focus:border-blue-500 focus:bg-gray-800 focus:outline-none' value  = {Email} onChange = {(e) =>{
-              setEmail(e.target.value)}} autoComplete = 'false'></input>
-          </div>
+    <>
+      <div className="grid grid-cols-1 sm:grid-cols-2 h-screen w-full">
+        <div className="bg-gray-800 flex flex-col justify-center">
+          <form
+            className="max-w-[400px] w-full mx-auto bg-gray-900 p-8 px-8 rounded-lg"
+            action={addEntry}
+            method="post"
+          >
+            <h2 className="text-4xl dark:text-white font-bold text-center">
+              Login
+            </h2>
+            <div className="flex flex-col text-gray-400 py-2">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                className="rounded-lg mt-2 py-2 px-4 bg-gray-700 focus:border-blue-500 focus:bg-gray-800 focus:outline-none"
+                value={Email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+                autoComplete="false"
+              ></input>
+            </div>
 
-          <div className='flex flex-col text-gray-400 py-2'>
-            <label htmlFor='password'>Password</label>
-            <input type="password" className='rounded-lg mt-2 pt-2 bg-gray-700 focus:border-blue-500 focus:bg-gray-800 focus:outline-none' value={Password} onChange = {(e) =>{setPassword(e.target.value)}} autoComplete = 'false'></input>
-          </div>
-          <div className='flex justify-between text-gray-400 py-2'>
-            <p className='flex items-center'><input className = 'mr-2' type="checkbox"></input>Remember me</p>
-            <p>Forget password?</p>
-          </div>
-          <Link to ={'/Homepage'}><button className='rounded-lg w-full my-5 py-2 bg-teal-500 shadow-lg shadow-teal-300/30 hover: shadow-teal-500/40 text-white font-semibold' onClick={postEntry}>Sign in</button></Link>
-          <Link to = {'/Register'}><button className='rounded-lg w-full my-5 py-2 bg-teal-500 shadow-lg shadow-teal-300/30 hover: shadow-teal-500/40 text-white font-semibold'>New User? Register here</button></Link>  
-        </form>
-      </div>
+            <div className="flex flex-col text-gray-400 py-2">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                className="rounded-lg mt-2 py-2 px-4 bg-gray-700 focus:border-blue-500 focus:bg-gray-800 focus:outline-none"
+                value={Password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+                autoComplete="false"
+              ></input>
+            </div>
+            <div className="flex justify-between text-gray-400 py-2">
+              {/* <p className="flex items-center">
+              <input className="mr-2" type="checkbox"></input>Remember me
+            </p> */}
+              <p className="underline">Forget password?</p>
+            </div>
+            <Link to={"/Homepage"}>
+              <button
+                className="rounded-lg w-full my-5 py-2 bg-teal-500 shadow-lg shadow-teal-300/30 hover: shadow-teal-500/40 text-white font-semibold"
+                onClick={postEntry}
+                disabled={isLoading}
+              >
+                {isLoading ? <BeatLoader color="white" /> : "Sign In"}
+              </button>
+            </Link>
+            {/* <Link to={"/Register"}>
+            <button className="rounded-lg w-full my-5 py-2 bg-teal-500 shadow-lg shadow-teal-300/30 hover: shadow-teal-500/40 text-white font-semibold">
+              New User? Register here
+            </button>
+          </Link> */}
+          </form>
+        </div>
 
-      <div className='hidden sm:block'>
-        <img className='w-full h-full object-cover' src={loadImg} alt =""></img>
+        <div className="hidden sm:block">
+          <img
+            className="w-full h-full object-cover"
+            src={loadImg}
+            alt=""
+          ></img>
+        </div>
       </div>
-    </div>
-  )
-}
+      <ToastContainer
+        position="bottom-left"
+        autoClose={4000}
+        hideProgressBar={false}
+        closeOnClic={true}
+        pauseOnHover={true}
+        draggable={true}
+        progress={undefined}
+        theme="dark"
+      />
+    </>
+  );
+};
--- student-front/src/components/Homepage/Homepage1.jsx
+++ student-front/src/components/Homepage/Homepage1.jsx
@@ -1,172 +1,207 @@
-import React from 'react'
-import data from './data.json'
-import loadImg from '../Form/1.jpeg'
-import Cards from './Cards';
-import Input from './Input.json'
-import Input1 from './Input1.json'
-import { Navbar } from '../Navbar/Navbar';
-import {useEffect, useState} from 'react'
-import { json } from 'react-router-dom'
-import crypto from 'crypto-js'
-import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import { Pre_loading_page } from '../Pre-Loading/Pre_loading_page';
+import React from "react";
+import { Navbar } from "../Navbar/Navbar";
+import { useEffect, useState } from "react";
+import crypto from "crypto-js";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Pre_loading_page } from "../Pre-Loading/Pre_loading_page";
 import axios from "axios";
-import getStuId from "../../utils/getStuId"
+import getStuId from "../../utils/getStuId";
 
 export const Homepage1 = () => {
-
-    let url = "http://127.0.0.1:5000/api"
-    let navigate = useNavigate();
-    const [_data, set_data] = useState({});
-    const [isLoading, setIsLoading] = useState(true);
-    useEffect(() => {
-        console.log("hello from homepage")
-    
-        const getValues = async () => {
-          try {
-            const {data} = await axios.get(`/api/company/of/${getStuId()}`, {withCredentials : true})
-            console.log("Data from company page: ", data)
-            set_data(data.data)
-            setIsLoading(false)
-            console.log("Data stored in useState: ", _data)
-          } catch (err) {
-            console.log("error: ", err)
-          }
-        }
-
-        getValues()
-        return () => {
-    
-        }
-      }, [])
-
-      function convertToDate(dateString){
-        const date = new Date(dateString);
-        const day = date.getDate().toString().padStart(2, '0');
-        const month = (date.getMonth() + 1).toString().padStart(2, '0');
-        const year = date.getFullYear().toString();
-        const formattedDate = `${day}-${month}-${year}`;
-        return formattedDate;
+  let navigate = useNavigate();
+  const [_data, set_data] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const getValues = async () => {
+      try {
+        const { data } = await axios.get(`/api/company/of/${getStuId()}`, {
+          withCredentials: true,
+        });
+        set_data(data.data);
+        setIsLoading(false);
+      } catch (err) {
+        console.log("error: ", err);
       }
+    };
 
-      function encrypter(data){
-        const encodedWord = crypto.enc.Utf8.parse(data); // e
-        var encoded = crypto.enc.Base64.stringify(encodedWord);
-        encoded = encoded.replace(/\+/g,'p23S').replace(/\//g,'sL3S4').replace(/=/g,'e2uAl');
-        return encoded;
-      }
+    getValues();
+    return () => {};
+  }, []);
 
+  function convertToDate(dateString) {
+    const date = new Date(dateString);
+    const day = date.getDate().toString().padStart(2, "0");
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const year = date.getFullYear().toString();
+    const formattedDate = `${day}-${month}-${year}`;
+    return formattedDate;
+  }
 
-      function redirect(data){  
-        console.log("hello from redirect")
-        const encrypt = encrypter(data);
-        console.log("data from encryption: " + encrypt)
-        navigate(`/Company/${encrypt}`)
-      }
+  function encrypter(data) {
+    const encodedWord = crypto.enc.Utf8.parse(data); // e
+    var encoded = crypto.enc.Base64.stringify(encodedWord);
+    encoded = encoded
+      .replace(/\+/g, "p23S")
+      .replace(/\//g, "sL3S4")
+      .replace(/=/g, "e2uAl");
+    return encoded;
+  }
 
-    const renderItem1 = (item) => {
-        return (
-            
-            <div className=' p-4 mx-10 my-8 text-white rounded-lg' style={{ backgroundColor: '#1A1C33' }}>
-
-                <div>
-                    <h3><b>{item.name}</b></h3>
-                </div>
-
-                <br />
-
-                <div className='flex flex-row justify-between'>
-                    <div>
-                        <label><b>{item.website}</b></label>
-                    </div>
-
-                    <div>
-                        <label>Mail to: </label><label><u><b>{item.email}</b></u></label>
-                    </div>
-                </div>
-
-                <br />
-
-                <div className=" mx-auto container bg-white dark:bg-gray-800 shadow rounded">
-
-                <table className="min-w-full bg-white dark:bg-gray-800">
-                    <thead>
-                        <tr className="w-full h-16 border-gray-300 border-b py-8">
-
-                            <th role="columnheader" className="px-5 py-3 text-gray-600 dark:text-gray-400 font-normal pr-6 text-left text-sm tracking-normal leading-4">Role</th>
-                            <th role="columnheader" className="px-5 py-3 text-gray-600 dark:text-gray-400 font-normal pr-6 text-left text-sm tracking-normal leading-4">AVG. Package</th>
-                            <th role="columnheader" className="px-5 py-3 text-gray-600 dark:text-gray-400 font-normal pr-6 text-left text-sm tracking-normal leading-4">TYPE</th>
-                            <th role="columnheader" className="px-5 py-3 text-gray-600 dark:text-gray-400 font-normal pr-6 text-left text-sm tracking-normal leading-4">DEADLINE</th>
-                            <th role="columnheader" className="px-5 py-3 text-gray-600 dark:text-gray-400 font-normal pr-6 text-left text-sm tracking-normal leading-4">INTERVIEW DATE</th>
-                            <th role="columnheader" className="px-5 py-3 text-gray-600 dark:text-gray-400 font-normal pr-6 text-left text-sm tracking-normal leading-4">MODE</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            item.roles.map((item1) =>{
-                                return (
-                                    <React.Fragment>
-                                        <tr className="h-24 border-gray-300 border-b">
-                                            <td className="px-5 py-3 text-sm pr-6 whitespace-no-wrap text-gray-800 dark:text-gray-100 tracking-normal leading-4">{item1.name}</td>
-                        
-                                            <td className="px-5 py-3 text-sm pr-6 whitespace-no-wrap text-gray-800 dark:text-gray-100 tracking-normal leading-4">{item1.avgPackage} LPA</td>
-                        
-                                            <td className="px-5 py-3 text-sm pr-6 whitespace-no-wrap text-gray-800 dark:text-gray-100 tracking-normal leading-4">{item1.type}</td>
-
-                                            <td className="px-5 py-3 text-sm pr-6 whitespace-no-wrap text-gray-800 dark:text-gray-100 tracking-normal leading-4">{convertToDate(item1.deadline)}</td>
-                                    
-                                            <td className="px-5 py-3 text-sm pr-6 whitespace-no-wrap text-gray-800 dark:text-gray-100 tracking-normal leading-4">{convertToDate(item1.interviewDate)}</td>
-
-                                            <td className="px-5 py-3 text-sm pr-6 whitespace-no-wrap text-gray-800 dark:text-gray-100 tracking-normal leading-4">{item1.mode}</td>
-                                        </tr>
-                                    </React.Fragment>
-                                )
-                            })
-                        }
-                    </tbody>
-                </table>
-            </div>
-
-            <br />
-                <div className='grid justify-items-center'>
-                    <div className='bg-white text-black w-1/4 rounded-lg grid justify-items-center'>
-                       <Link to={'/Company/' + encrypter(item._id)}><button>View More</button></Link>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+  const renderItem1 = (item) => {
     return (
+      <div
+        className=" p-4 mx-10 my-8 text-white rounded-lg"
+        style={{ backgroundColor: "#1A1C33" }}
+      >
         <div>
-        {isLoading ? <Pre_loading_page/> : 
-        <div style={{ backgroundColor: '#0B0E2A' }}>
-
-            <Navbar />
-
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-
-            <div className='flex justify-center'>
-                <div  className='w-2/12 grid justify-items-center m-2 p-2 hover:bg-blue-700 bg-indigo-900'>
-                    <button className='text-white' >Full Time Job</button>
-                </div>
-
-
-                <div className='w-2/12 grid justify-items-center m-2 p-2 hover:bg-blue-700 bg-indigo-900'>
-                    <button className='text-white hover:bg-#2538E6' >Internship</button>
-                </div>11
-            </div>
-
-            <br />
-            <br />
-            <br />
-            { _data.map(renderItem1)}  
+          <h3>
+            <b>{item.name}</b>
+          </h3>
         </div>
-}
+
+        <br />
+
+        <div className="flex flex-row justify-between">
+          <div>
+            <label>
+              <b>{item.website}</b>
+            </label>
+          </div>
+
+          <div>
+            <label>Mail to: </label>
+            <label>
+              <u>
+                <b>{item.email}</b>
+              </u>
+            </label>
+          </div>
+        </div>
+
+        <br />
+
+        <div className=" mx-auto container bg-white dark:bg-gray-800 shadow rounded">
+          <table className="min-w-full bg-white dark:bg-gray-800">
+            <thead>
+              <tr className="w-full h-16 border-gray-300 border-b py-8">
+                <th
+                  role="columnheader"
+                  className="px-5 py-3 text-gray-600 dark:text-gray-400 font-normal pr-6 text-left text-sm tracking-normal leading-4"
+                >
+                  Role
+                </th>
+                <th
+                  role="columnheader"
+                  className="px-5 py-3 text-gray-600 dark:text-gray-400 font-normal pr-6 text-left text-sm tracking-normal leading-4"
+                >
+                  AVG. Package
+                </th>
+                <th
+                  role="columnheader"
+                  className="px-5 py-3 text-gray-600 dark:text-gray-400 font-normal pr-6 text-left text-sm tracking-normal leading-4"
+                >
+                  TYPE
+                </th>
+                <th
+                  role="columnheader"
+                  className="px-5 py-3 text-gray-600 dark:text-gray-400 font-normal pr-6 text-left text-sm tracking-normal leading-4"
+                >
+                  DEADLINE
+                </th>
+                <th
+                  role="columnheader"
+                  className="px-5 py-3 text-gray-600 dark:text-gray-400 font-normal pr-6 text-left text-sm tracking-normal leading-4"
+                >
+                  INTERVIEW DATE
+                </th>
+                <th
+                  role="columnheader"
+                  className="px-5 py-3 text-gray-600 dark:text-gray-400 font-normal pr-6 text-left text-sm tracking-normal leading-4"
+                >
+                  MODE
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {item.roles.map((item1) => {
+                return (
+                  <React.Fragment>
+                    <tr className="h-24 border-gray-300 border-b">
+                      <td className="px-5 py-3 text-sm pr-6 whitespace-no-wrap text-gray-800 dark:text-gray-100 tracking-normal leading-4">
+                        {item1.name}
+                      </td>
+
+                      <td className="px-5 py-3 text-sm pr-6 whitespace-no-wrap text-gray-800 dark:text-gray-100 tracking-normal leading-4">
+                        {item1.avgPackage} LPA
+                      </td>
+
+                      <td className="px-5 py-3 text-sm pr-6 whitespace-no-wrap text-gray-800 dark:text-gray-100 tracking-normal leading-4">
+                        {item1.type}
+                      </td>
+
+                      <td className="px-5 py-3 text-sm pr-6 whitespace-no-wrap text-gray-800 dark:text-gray-100 tracking-normal leading-4">
+                        {convertToDate(item1.deadline)}
+                      </td>
+
+                      <td className="px-5 py-3 text-sm pr-6 whitespace-no-wrap text-gray-800 dark:text-gray-100 tracking-normal leading-4">
+                        {convertToDate(item1.interviewDate)}
+                      </td>
+
+                      <td className="px-5 py-3 text-sm pr-6 whitespace-no-wrap text-gray-800 dark:text-gray-100 tracking-normal leading-4">
+                        {item1.mode}
+                      </td>
+                    </tr>
+                  </React.Fragment>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+
+        <br />
+        <div className="grid justify-items-center">
+          <div className="bg-white text-black w-1/4 rounded-lg grid justify-items-center">
+            <Link to={"/Company/" + encrypter(item._id)}>
+              <button>View More</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  return (
+    <div>
+      {isLoading ? (
+        <Pre_loading_page />
+      ) : (
+        <div style={{ backgroundColor: "#0B0E2A" }}>
+          <Navbar />
+
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+
+          <div className="flex justify-center">
+            <div className="w-2/12 grid justify-items-center m-2 p-2 hover:bg-blue-700 bg-indigo-900">
+              <button className="text-white">Full Time Job</button>
+            </div>
+            <div className="w-2/12 grid justify-items-center m-2 p-2 hover:bg-blue-700 bg-indigo-900">
+              <button className="text-white hover:bg-#2538E6">
+                Internship
+              </button>
+            </div>
+            11
+          </div>
+
+          <br />
+          <br />
+          <br />
+          {_data.map(renderItem1)}
+        </div>
+      )}
     </div>
-    )
-}
+  );
+};
